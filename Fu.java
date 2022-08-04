52af9f82207f7a2438c310157215133c77f503ec
liang_www <105441821+liang666ww@users.noreply.github.com>
2022-08-04 23:32:44 +0800

    Create Fu.java

---
+++ Fu.java
@@ -0,0 +1,13 @@
+package super内存图;
+
+public class Fu {
+    public int age = 40;
+    //无参构造方法
+    public Fu() {
+        System.out.println("Fu类无参方法被调用");
+    }
+    //无参成员方法
+    public void method(){
+        System.out.println("method方法被调用");
+    }
+}
